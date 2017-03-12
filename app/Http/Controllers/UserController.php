fdd4a2e819c129139a7a0f89eb8097e95be74fe0
Rushanthan <rushan26@live.com>
2017-03-12 20:31:57 +0530

    Model and Controller created for User Module

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function postSignUp(Request $request)
+    {
+        
+    }
+    
+    public function postSignIn(Request $request)
+    {
+        
+    }
+}
