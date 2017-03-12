4c47e9184cc9bed8b986f866b9a7bd9257b8342e
Rushanthan <rushan@live.com>
2017-03-12 18:50:19 +0530

    Sign In and Sign Up forms created

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,5 +5,36 @@ Welcome to Laravel
 @endsection
 
 @section('content')
-Hi! this is my first laravel application
+<div class="row">
+    <div class="col-md-6">
+        <form action="" method="post">
+            <div class="form-group">
+                <label for="email">Your Email</label>
+                <input type="email" class="form-control" name="email" id="email" />
+            </div>
+            <div class="form-group">
+                <label for="firstname">Your Firstname</label>
+                <input type="text" class="form-control" name="firstname" id="firstname" />
+            </div>
+            <div class="form-group">
+                <label for="password">Your Password</label>
+                <input type="password" class="form-control" name="password" id="password" />
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+    <div class="col-md-6">
+        <form action="" method="post">
+            <div class="form-group">
+                <label for="email">Your Email</label>
+                <input type="email" class="form-control" name="email" id="email" />
+            </div>
+            <div class="form-group">
+                <label for="password">Your Password</label>
+                <input type="password" class="form-control" name="password" id="password" />
+            </div>
+            <button type="submit" class="btn btn-primary">Sign In</button>
+        </form>
+    </div>
+</div>    
 @endsection
